90acdb1fd716058eb5cd879c51ab5041c375f3c2
corsicanu <me@corsicanu.ro>
2021-04-08 09:20:55 +0300

    BoardConfig.mk: simplify device variables
    Change-Id: I7179bcff6de44c966996400135087a5402d0d7a7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 # Platform
-DEVICE_PATH := device/samsung/beyond1lte
+DEVICE_CODENAME := beyond1lte
+DEVICE_PATH := device/samsung/$(DEVICE_CODENAME)
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
 TARGET_BOARD_PLATFORM_GPU := mali-g76
